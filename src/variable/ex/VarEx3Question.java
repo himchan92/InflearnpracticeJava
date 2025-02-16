329e77391d15a4d37c4e4115de987c28a05ec6cb
omea92 <thdjun92@naver.com>
2025-02-16 11:55:34 +0900

    변수 단원 문제풀이

---
+++ src/variable/ex/VarEx3Question.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3Question {
+
+    public static void main(String[] args) {
+        long num = 10000000000L;
+        boolean flag = true;
+
+        System.out.println(num);
+        System.out.println(flag);
+    }
+}
